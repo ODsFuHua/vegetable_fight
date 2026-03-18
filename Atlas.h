8fa3b0f352d78857e4e39b737731f1c5fa861c53
ODsFuHua <1293465796@qq.com>
2026-03-18 21:19:34 +0800

    Update Atlas.h

--- Atlas.h
+++ Atlas.h
@@ -1,10 +1,41 @@
-class Atlas{
+#include <vector>
+#include <graphics.h>
+
+class Atlas {
+public:
     Atlas() = default;
     ~Atlas() = default;
 
-    void load_from_file(LPCTSTR path_template, int number){
-        _img_list.clear();
-        _img_list.resize(number);
-        
+    void load_from_file(LPCTSTR path_template, int number) {
+        img_list.clear();
+        img_list.resize(number);
+
+        TCHAR path_file[256];
+        for (int i = 0, i < number, i++) {
+            _stprintf_s(path_file, path_template, i + 1);
+            loadimage(&img_list, path_file);
+        }
     }
-}
+    
+    void clear() {
+        img_list.clear();
+    }
+
+    void get_size() {
+        return (int)img_list.size();
+    }
+
+    IMAGE* get_image(int s_num) {
+        if (s_num < 0 || s_num >= img_list.size()) {
+            return nullptr;
+        }
+        return &img_list[s_num];
+    }
+
+    void add_image(const IMAGE& img) {
+        img_list.push_back(img);
+    }
+
+private:
+    std::vector<IMAGE> img_list;
+};
