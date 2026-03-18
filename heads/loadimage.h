8a2504568b79b782e1538e3cc106245755824d4f
ODsFuHua <1293465796@qq.com>
2026-03-18 22:57:05 +0800

    update

---
+++ heads/loadimage.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <graphics.h>
+
+IMAGE img_menu_background;
+IMAGE img_vs;
+IMAGE img_1P;
+IMAGE img_2P;
+IMAGE img_1P_control_desc;
+IMAGE img_2P_control_desc;
+IMAGE img_gravestone_left;
+IMAGE img_gravestone_right;
+IMAGE img_selector_tip;
+IMAGE img_selector_background;
+IMAGE img_
