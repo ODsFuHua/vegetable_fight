9f0c1c575f91e4414028a2ac6fa120c611cfa61d
ODsFuHua <1293465796@qq.com>
2026-03-19 20:46:44 +0800

    1

--- heads/loadimage.h
+++ heads/loadimage.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <graphics.h>
+#include <Atlas.h>
 
 IMAGE img_menu_background;
 IMAGE img_vs;
@@ -11,4 +12,60 @@ IMAGE img_gravestone_left;
 IMAGE img_gravestone_right;
 IMAGE img_selector_tip;
 IMAGE img_selector_background;
-IMAGE img_
+IMAGE img_1P_selector_botton_up_left;
+IMAGE img_1P_selector_botton_up_right;
+IMAGE img_1P_selector_botton_down_left;
+IMAGE img_1P_selector_botton_down_right;
+IMAGE img_2P_selector_botton_up_left;
+IMAGE img_2P_selector_botton_up_right;
+IMAGE img_2P_selector_botton_down_left;
+IMAGE img_2P_selector_botton_down_right;
+IMAGE img_peashooter_selector_background_left;
+IMAGE img_peashooter_selector_background_right;
+IMAGE img_sunflower_selector_background_left;
+IMAGE img_sunflower_selector_background_right;
+
+IMAGE img_sky;
+IMAGE img_hills;
+IMAGE img_platform_large;
+IMAGE img_platform_small;
+
+IMAGE img_1P_cursor;
+IMAGE img_2P_cursor;
+
+Atlas atlas_peashooter_stand_left;
+Atlas atlas_peashooter_stand_right;
+Atlas atlas_peashooter_run_left;
+Atlas atlas_peashooter_run_right;
+Atlas atlas_peashooter_ex_left;
+Atlas atlas_peashooter_ex_right;
+Atlas atlas_peashooter_die_left;
+Atlas atlas_peashooter_die_right;
+
+Atlas atlas_sunflower_stand_left;
+Atlas atlas_sunflower_stand_right;
+Atlas atlas_sunflower_run_left;
+Atlas atlas_sunflower_run_right;
+Atlas atlas_sunflower_ex_left;
+Atlas atlas_sunflower_ex_right;
+Atlas atlas_sunflower_die_left;
+Atlas atlas_sunflower_die_right;
+
+IMAGE img_pea;
+Atlas atlas_pea_break;
+Atlas atlas_sun;
+Atlas atlas_sun_explode;
+Atlas atlas_sun_ex;
+Atlas atlas_sun_ex_explode;
+Atlas atlas_sun_text;
+
+Atlas atlas_run_effect;
+Atlas atlas_jump_effect;
+Atlas atlas_land_effect;
+
+IMAGE img_1P_win;
+IMAGE img_2P_win;
+IMAGE img_win_background;
+
+IMAGE img_peashooter_avatar;
+IMAGE img_sunflower_avatar;
